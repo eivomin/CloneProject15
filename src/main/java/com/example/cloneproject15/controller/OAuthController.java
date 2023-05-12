b19b80c80550fca29f49b8d1c8c8e08b0c856402
tiktokyi <tiktokyi@gmail.com>
2023-05-13 00:19:43 +0900

    [Chore] 스웨거 추가

--- src/main/java/com/example/cloneproject15/controller/OAuthController.java
+++ src/main/java/com/example/cloneproject15/controller/OAuthController.java
@@ -1,5 +1,9 @@
 package com.example.cloneproject15.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@Tag(name = "OAuthController", description = "OAuth Controller")
 public class OAuthController {
 
+    @Operation(summary = "카카오callback API" , description = "카카오 callback")
+    @ApiResponses(value ={@ApiResponse(responseCode= "200", description = "회원 가입 완료" )})
     @GetMapping("/oauth/kakao")
     public void kakaoCallback(@RequestParam String code){
         System.out.println(code);
