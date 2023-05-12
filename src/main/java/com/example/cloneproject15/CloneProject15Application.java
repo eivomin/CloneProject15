bb9fb8db61638655e734b60fb07315664b9c4ffb
eivomin <eivomiin@gmail.com>
2023-05-12 10:11:04 +0900

    Initial commit

---
+++ src/main/java/com/example/cloneproject15/CloneProject15Application.java
@@ -0,0 +1,13 @@
+package com.example.cloneproject15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneProject15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneProject15Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cloneproject15/CloneProject15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloneproject15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneProject15ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
